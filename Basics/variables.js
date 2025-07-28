f46952b70560f2c21639b5faa4be1446325e0d93
kapil903 <bikhil098@gmail.com>
2025-07-28 07:32:00 +0000

    variables and Datatypes

---
+++ Basics/variables.js
@@ -0,0 +1,11 @@
+const accountId = 23455
+let accountName  = "Kapil"
+accountCity = "Delhi"
+
+let accountNo;
+
+console.table([accountId, accountName, accountCity, accountNo])
+
+console.log(typeof null) //Object
+
+console.log(typeof undefined) //Undefined
