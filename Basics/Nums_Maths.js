8b91ab9beb85bd480f988ec4f166ecff5ce02745
kapil903 <bikhil098@gmail.com>
2025-07-28 10:26:16 +0000

    Maths and num

---
+++ Basics/Nums_Maths.js
@@ -0,0 +1,40 @@
+// let score = 100
+
+// let balance = new Number(400)
+
+// console.log(score)
+// console.log(balance)
+
+// console.log(balance.toString())
+
+// console.log( typeof (balance.toString()))
+
+// console.log(balance.toFixed(2))
+
+// const othernumber = 345.678
+
+// console.log(othernumber.toPrecision(4))
+
+// const hundreds = 10456783;
+
+// console.log(hundreds.toLocaleString('en-IN'))
+
+//++++++++++++++++++++ MATH +++++++++++++++
+// console.log(Math.abs(-4))
+
+// console.log(Math.round(4.8))
+
+// console.log(Math.ceil(4.2)) //Always gives upper value
+
+// console.log(Math.floor(4.9)) //Always giives lower value
+
+console.log(Math.random())
+
+console.log((Math.random()*10)+1)
+
+console.log(Math.floor((Math.random()*10)+1))
+
+const min = 10
+const max = 20
+
+console.log(Math.floor(Math.random()*(max-min+1))+min)
