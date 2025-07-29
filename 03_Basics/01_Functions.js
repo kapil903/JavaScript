c412c1c9f0a938c78e39ff5d2eafe5b573a69998
kapil903 <bikhil098@gmail.com>
2025-07-29 13:18:46 +0000

    function in objects and array

--- 03_Basics/01_Functions.js
+++ 03_Basics/01_Functions.js
@@ -8,5 +8,22 @@ const  result = addTwoNumber(3,4)
 function loginUserMessage(username = "Sam"){
     return `${username} just logged in`
 }
-console.log(loginUserMessage());
-console.log(loginUserMessage("Kapil"));
+// console.log(loginUserMessage());
+// console.log(loginUserMessage("Kapil"));
+
+function calculateCartPrice(...num1){ //Rest or Spread Operator
+    return num1
+}
+
+// console.log(calculateCartPrice(100,300,200))
+
+const user = {
+    username : "kapil", 
+    price : 100
+}
+
+function handleObject(anyobject){ //Function in Object
+    console.log(`Username is ${anyobject.username} and price is ${anyobject.price}` );
+}
+
+handleObject(user)
