408ff3c3b8b3d1ac543ca5e03f39e7baf2790449
kapil903 <bikhil098@gmail.com>
2025-09-06 17:14:31 +0000

    Bindig Objects

---
+++ Advance_js/MathPI.js
@@ -0,0 +1,26 @@
+const descriptor =  Object.getOwnPropertyDescriptor(Math, 'PI');
+console.log(descriptor);
+
+const chai = {
+    name: "Ginger tea",
+    price : 250, 
+    isAvailable: true,
+    orderchai : function(){
+        console.log(`chai nhi bani`)
+    }
+}
+
+console.log(Object.getOwnPropertyDescriptor(chai, "name"));
+
+Object.defineProperty(chai, "name", {
+    writable : false,
+    enumerable : false
+})
+
+console.log(Object.getOwnPropertyDescriptor(chai, "name"));
+
+for (const [key, value] of Object.entries(chai)) {
+    if(typeof value !== "function"){
+        console.log(key, value);
+    }
+}
